8c0b10dd6d11ea09a74c3b0d8a989202e8e4bd6d
Huy Vu <43260621+HuyVu0508@users.noreply.github.com>
2021-08-31 10:10:16 -0400

    Add files via upload

---
+++ Prefabs/portal/PortalSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalSound : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Prefabs/portal_intro/PlayAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAnimation : MonoBehaviour
+{
+    private Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        anim.Play();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
